1dfc9f749b980706b71cae7e5b4872cb052c8a15
VanQuyet04 <truongquyet137@gmail.com>
2024-11-23 23:41:12 +0700

    edit access

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,12 @@
 // src/firebase.js
+const fs = require('fs');
 const admin = require('firebase-admin');
-const serviceAccount = require('../service/staynowapp1-firebase-adminsdk.json');
+
+// const serviceAccount = require('../service/staynowapp1-firebase-adminsdk.json');
+// Đọc file bí mật từ /etc/secrets
+const serviceAccount = JSON.parse(
+  fs.readFileSync('/etc/secrets/firebase-admin-sdk.txt', 'utf8')
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
