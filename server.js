81074d889c789322cb3862b8c464724f560bd7b9
VanQuyet04 <truongquyet137@gmail.com>
2024-12-04 14:19:05 +0700

    ping server

--- server.js
+++ server.js
@@ -16,14 +16,14 @@ app.use('/otp', otpRoutes)
 app.use('/api', createOrderRoute);
 app.use('/api', callbackRoute);
 // Ping chính server để giữ cho nó không bị ngủ
-// const interval = 10 * 60 * 1000; // 10 phút
+const interval = 10 * 60 * 1000; // 10 phút
 
-// setInterval(() => {
-//     fetch('https://staynow-server.onrender.com')
-//         .then(res => res.text())
-//         .then(text => console.log('Ping thành công:', text))
-//         .catch(err => console.error('Lỗi khi ping:', err));
-// }, interval);
+setInterval(() => {
+    fetch('https://staynow-server.onrender.com')
+        .then(res => res.text())
+        .then(text => console.log('Ping thành công:', text))
+        .catch(err => console.error('Lỗi khi ping:', err));
+}, interval);
 
 const port = config.PORT || 5000;
 server.listen(port, () => {
